2c94905930aaf28cf4c1d80dc8663e034f405d59
Blanca Hidalgo <b.hidalgo.ley@gmail.com>
2023-02-28 18:35:11 +1100

    changed some code in my last variable

--- Develop/script.js
+++ Develop/script.js
@@ -17,7 +17,7 @@
  // This is hte event listener
  generateBtn.addEventListener("click", generatePassword);
 
- function confirmGenerate () {
+ function confirmGenerate() {
   var generatePassword 
   if (confirm("Do you want to generate a secure password? - Click to go ahead") === true) {
     console.log("Process started!");
@@ -28,7 +28,7 @@
 
  function confirmGenerate() {}
  // 1. Password Length
- var passwordLength = prompt("Your new password should have between 8 - 128 characters. How many would you like?");
+ var passwordLength = prompt("Your new password should contain between 8 - 128 characters. How many would you like?");
 //error null if chose outside those parameters ^^
  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
   alert("Not valid - please choose a number between 8 - 128");
@@ -41,42 +41,41 @@ if (passwordLength > 8 || passwordLength < 128 ||
     console.log(passwordLength);
     alert("You have selected a password length of "  + passwordLength + "  characters");
     //include numbers 
-    var includeNum = confirm("Would you like to include numbers? Click 'Yes' or 'No'");
+    var includeNum = confirm("Would you like to include numbers? Click 'OK' or 'CANCEL'");
     //include special characters
-    var includeSpecialChar = confirm("Would you like any special characters? Click 'Yes' or 'No'");
+    var includeSpecialChar = confirm("Would you like any special characters? Click 'OK' or 'CANCEL'");
     //include upperCase
-    var includeupperCase = confirm("Would you like any special upper case letters? Click 'Yes' or 'No'"); 
+    var includeupperCase = confirm("Would you like any special upper case letters? Click 'OK' or 'CANCEL'"); 
     //include lowerCase
-    var includelowerCase = confirm("Would you like any special lower case letters? Click 'Yes' or 'No'"); 
+    var includelowerCase = confirm("Would you like any special lower case letters? Click 'OK' or 'CANCEL'"); 
 
   if (!includeNum && !includeSpecialChar && !includeUppercase && !includelowercase) {
     return alert("Not valid - choose one character of each type at least to proceed");
   }
   } else {
-    console.log("Password Generator Candelled, BYE!");
+    console.log("Password Generator Cancelled, BYE!");
   }
+  var charSet = "";
+  if (includelowerCase===true) {
+    charSet = charSet + lowerCase;
+  }
+  if (includeupperCase===true) {
+    charSet = charSet + upperCase;
+  }
+  if (includeNum===true) {
+    charSet = charSet + num;
+  }
+  if (includeSpecialChar===true) { 
+    charSet = charSet + specialChar;
+  }
+
+  console.log(charSet)
+
+  var retVal = "";
+  for (var i = 0; i < parseInt(passwordLength); i++) {
+  retVal += charSet[Math.floor(Math.random()*charSet.length)];
+  console.log(retVal);
+  }
+
+  return retVal;
  }
-
-// {
-//   var charSet = "";
-//   if (Low===true) {
-//     charSet = charSet + charTypes.lowerCase;
-//   }
-//   if (Upp===true) {
-//     charSet = charSet + charTypes.upperCase;
-//   }
-//   if (hasNumbers===true) {
-//     charSet = charSet + charTypes.numeric;
-//   }
-//   if (hasSpecialCharacters===true) { 
-//     charSet = charSet + charTypes.special;
-//   }
-
-//   var retVal = "";
-//   for (var i = 0; i < parseInt(password.Length); i++) {
-//   retVal += charSet[Math.floor(Math.random()*charSet.lenght)];
-//   console.log(retVal);
-//   }
-
-//   return retVal;
-// }
